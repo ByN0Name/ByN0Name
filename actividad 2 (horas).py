21bde24ec9b1a0712ec3352d6716a3d6f7566ea9
ByN0Name <91593732+ByN0Name@users.noreply.github.com>
2021-09-29 10:10:21 +0200

    Create actividad 2 (horas).py

---
+++ actividad 2 (horas).py
@@ -0,0 +1,10 @@
+A = 0
+B = 0
+C = 0
+
+A = int(input("Escriba las horas "))
+B = int(input("Escriba los minutos "))
+C = int(input("Escriba los segundos "))
+
+D = A*3600 + B*60 + C
+print("Han pasado" , D ,"segundos")
