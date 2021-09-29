24526d7e47965a824f32f4f2aa84faa7e3ecff71
ByN0Name <91593732+ByN0Name@users.noreply.github.com>
2021-09-29 10:07:52 +0200

    Create actividad 1 a).py

---
+++ actividad 1 a).py
@@ -0,0 +1,9 @@
+A = 0
+B = 0
+while(A == B):
+    A = int(input("introduzca un valor A "))
+    B = int(input("introduzca otro valor B "))
+if(A < B):
+    print("A es el menor")
+else:
+    print("B es el menor")
