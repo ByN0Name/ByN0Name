967741f5893876f2b57698c0f4dd3a3c6902b387
ByN0Name <91593732+ByN0Name@users.noreply.github.com>
2021-09-29 10:06:45 +0200

    Create actividad 1.py

---
+++ actividad 1.py
@@ -0,0 +1,8 @@
+B = 0
+while(A == B):
+    A = int(input("introduzca un valor A "))
+    B = int(input("introduzca otro valor B "))
+if(A > B):
+    print("A es el mayor")
+else:
+    print("B es el mayor")
