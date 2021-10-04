fbbaae4ee6858a8844e4ed096b92781dded3fee0
ByN0Name <91593732+ByN0Name@users.noreply.github.com>
2021-10-04 10:22:29 +0200

    Create estados del agua segun su temperatura.py

---
+++ estados del agua segun su temperatura.py
@@ -0,0 +1,10 @@
+a = 0
+
+a = int(input("Dime la temperatura "))
+
+if(a <= -20):
+    print("El agua esta en estado sólido")
+else:
+    print("El agua esta en estado liquido")
+if(a >= 150):
+    print("El agua esta en estado gaseoso")
