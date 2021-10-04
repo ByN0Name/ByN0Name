9b8209c5c0873b8d0bb349c5dd394558c4024c70
ByN0Name <91593732+ByN0Name@users.noreply.github.com>
2021-10-04 10:11:24 +0200

    Create NIvel de deporte según su edad.py

---
+++ NIvel de deporte según su edad.py
@@ -0,0 +1,10 @@
+a = 0
+a = int(input("Dime la edad que tienes"))
+if((a == 6) or (a == 7)):
+    print("benjamin")
+elif((a == 8) or (a == 9)):
+    print("Alevín")
+elif((a == 10) or (a == 11)):
+    print("Infantil")
+elif(a >= 12):
+    print("Cadete")
